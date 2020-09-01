9b8774eb5e16ad3d55c1b11b267010533f260a19
adarsh <adarshkalikadien@hotmail.com>
2020-09-01 11:47:31 +0200

    recursive program is now able to use system args, modified main

--- main.py
+++ main.py
@@ -5,17 +5,23 @@
 #                                                     #
 from find_centroid import Substituent
 from generate_tetrahedron import Complex
+import sys
 
 
 def main():
-    # make methyl class to find bond between C-H
-    methyl = Complex('substituents_xyz/manually_generated/CH3.xyz')
-    # Write to .xyz file and visualize, H is in center and C is pointing outwards and will be attached to catalyst
-    methyl.generate_substituent_and_write_xyz('CH4', 'H', 'H', 'H', True, False)
-    # find centroid for this newly generated substituent
-    substituent = Substituent('substituents_xyz/automatically_generated/CH4.xyz', 1.7)
-    centroid, indices = substituent.first_coordination()
-    print(centroid)
+    source_file = sys.argv[1]
+    target_molecule = sys.argv[2]
+    functionalization_list = sys.argv[3]
+    recursive_or_intial = sys.argv[4]
+    substituent_1 = sys.argv[5]
+    substituent_2 = sys.argv[6]
+    substituent_3 = sys.argv[7]
+    view_xyz_file = True if sys.argv[8].lower() == 'true' else False
+    save_xyz_picture = True if sys.argv[9].lower() == 'true' else False
+    
+    substituent = Complex(source_file, functionalization_list, recursive_or_intial)
+    substituent.generate_substituent_and_write_xyz(target_molecule, substituent_1, substituent_2, substituent_3,
+                                                   view_xyz_file, save_xyz_picture)
 
 
 if __name__ == "__main__":
