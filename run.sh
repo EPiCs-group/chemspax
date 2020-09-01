a49a640ed3aec10c8fa937e18ba67afe01768a9c
adarsh <adarshkalikadien@hotmail.com>
2020-09-01 20:14:17 +0200

    bash example working now, changed recursive mode to find functionalization sites in source file (previously in target file)

--- run.sh
+++ run.sh
@@ -8,32 +8,17 @@ echo "Looks like you want to add some substituents to a metal-ligand complex."
 echo "-------------------------------------------------------"
 
 
-SOURCE_FILE="skeletons/RuPNP_iPr.xyz"
-TARGET_FILE="substituents_xyz/automatically_generated/RuPNP_CH4.xyz"
-i=1
+SOURCE_FILE="skeletons/RuPNP_iPr_skl"
+TARGET_FILE="substituents_xyz/automatically_generated/RuPNP_CH4"
 
-if [ -f ${TARGET_FILE} ]
-then
-    echo ${TARGET_FILE} "exists already. Removing it first."
-    rm -rf ${TARGET_FILE}
-    cp skeletons/RuPNP_iPr.xyz ${TARGET_FILE}
-else
-    cp skeletons/RuPNP_iPr.xyz ${TARGET_FILE}
-fi
+rm substituents_xyz/automatically_generated/*.xyz
+rm visualizations/*.png
 
 echo "creating initial file"
-python main.py ${SOURCE_FILE} RuPNP_CH4 [[19,4],[23,4],[24,3],[20,3]] recursive H H C False False
+python main.py ${SOURCE_FILE}.xyz RuPNP_CH4_1 None recursive H H H False False
 
-until [ $i -gt 3 ]
+for i in `seq 1 3`
 do
   echo "Running recursive loop, #run:" ${i}
-  python main.py ${TARGET_FILE} RuPNP_CH4 None recursive H H C False False
-  ((i=i+1))
+  python main.py ${TARGET_FILE}_${i}.xyz RuPNP_CH4_$((i+1)) None recursive H H H False False
 done
-
-
-#else
-#    echo "Running loop"
-#    python main.py substituents_xyz/manually_generated/CH3.xyz CH4 [[1,0],[4,0]] initial H H C False False
-#    python main.py substituents_xyz/automatically_generated/CH4.xyz CH4 None recursive H H H True False
-#fi
