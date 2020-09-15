2e78ee2a2d30016d27bac18ee43982ac50dc0350
adarsh <adarshkalikadien@hotmail.com>
2020-09-15 16:20:05 +0200

    modified optimize script, added 6 functionalization sites for all skeletons

--- functionalize_and_optimize.sh
+++ functionalize_and_optimize.sh
@@ -11,18 +11,20 @@ echo "--------------------------------------------------------------------------
 #rm ../substituents_xyz/visualizations/*.png
 #
 CURRENT_DIR=$(pwd)
-SKELETON_LIST=$(cd skeletons && ls)
+SKELETON_LIST=$(cd skeletons && ls | cut -d '.' -f 1)
 # select 1 random substituent with C as central atom as starting point
 STARTING_C_SUBSTITUENT=$(cd substituents_xyz/manually_generated/ && ls -d C* | xargs shuf -n1 -e | cut -d '.' -f 1)
 # select 5 random substituents with C as central atom
 RANDOM_C_SUBSTITUENTS=$(cd substituents_xyz/manually_generated/ && ls -d C* | xargs shuf -n5 -e | cut -d '.' -f 1)
 
-SOURCE_FILE="/skeletons/RuPNP_iPr_skl"
-SOURCE_NAME=$(echo ${SOURCE_FILE} | cut -d '/' -f 3)
-TARGET_NAME=${SOURCE_NAME}_func
+#SOURCE_FILE="/skeletons/RuPNP_iPr_skl"
+#SOURCE_NAME=$(echo ${SOURCE_FILE} | cut -d '/' -f 3)
+#TARGET_NAME=${SOURCE_NAME}_func
 
 for skeleton in ${SKELETON_LIST}; do
 # loop over skeleton list and set index back to 1
+    SOURCE_FILE=/skeletons/${skeleton}
+    TARGET_NAME=${skeleton}_func
     i=1
     echo "creating initial file"
     # functionalize and optimize initial functionalized version of skeleton
@@ -32,7 +34,6 @@ for skeleton in ${SKELETON_LIST}; do
     # clean up mess and move relevant file to correct folder
     mv xtbop.xyz substituents_xyz/automatically_generated/optimized_structures/${TARGET_NAME}_${i}_opt
     rm -f xtbrestart
-    done
         for sub in ${RANDOM_C_SUBSTITUENTS}; do
         echo "Running recursive loop, run:" ${i}
         python attach_substituent_folder/main_attach_substituent.py substituents_xyz/automatically_generated/${TARGET_NAME}_${i}.xyz ${TARGET_NAME}_$((i+1)) ${sub} substituents_xyz/manually_generated/central_atom_centroid_database.csv 1.54
@@ -42,4 +43,5 @@ for skeleton in ${SKELETON_LIST}; do
         mv xtbop.xyz substituents_xyz/automatically_generated/optimized_structures/${TARGET_NAME}_$((i+1))_opt
         rm -f xtbrestart
         i=$((i+1))
+        done
 done
