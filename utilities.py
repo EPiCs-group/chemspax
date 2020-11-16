9e96926a7ba9982cae92d6afe5b0d4a884f7b9a8
adarsh <45177460+akalikadien@users.noreply.github.com>
2020-11-16 21:30:47 +0100

    added script and utility to remove hydrogens from a molecule

--- utilities.py
+++ utilities.py
@@ -457,6 +457,32 @@ def check_overlap(dataframe_xyz):
     return overlap_bool, overlap
 
 
+def remove_hydrogens_and_write(source_mol_file):
+    """Use Openbabel's meethods to delete all hydrogens of a molecule and return the same file type with the same
+    filename
+
+    :param source_mol_file:
+    :return: output file of molecule without hydrogens, same file type as input
+    """
+    # initalize openbabel classes
+    obconversion = openbabel.OBConversion()
+    # both xyz and mol can be used as input but mol contains an accurate graph representation
+    if source_mol_file[-4:] == '.mol':
+        obconversion.SetInFormat('mol')
+        obconversion.SetOutFormat('mol')
+    elif source_mol_file[-4:] == '.xyz':
+        obconversion.SetInFormat('xyz')
+        obconversion.SetOutFormat('xyz')
+    else:
+        raise Exception('file type is incorrect, .mol and .xyz are supported, not', source_mol_file[-4:])
+    # intialize class and read file
+    mol = openbabel.OBMol()
+    obconversion.ReadFile(mol, source_mol_file)
+    # remove all hydrogens and write file
+    mol.DeleteHydrogens()
+    obconversion.WriteFile(mol, source_mol_file)
+
+
 if __name__ == '__main__':
     # molec = 'H2O'
     # create_molecule_and_write_xyz('H2O', 'substituents_xyz/automatically_generated/' + molec + '.xyz')
