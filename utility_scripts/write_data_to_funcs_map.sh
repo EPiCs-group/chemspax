05adb08a10a91264ba69409af369ae74e9ba6019
adarsh <45177460+akalikadien@users.noreply.github.com>
2021-01-18 15:01:26 +0100

    modified write_data_to_funcs_map, all csvs are merged in 1 csv for data processing at the end

--- utility_scripts/write_data_to_funcs_map.sh
+++ utility_scripts/write_data_to_funcs_map.sh
@@ -52,3 +52,18 @@ mv ${CSV_FILE}_new ${CSV_FILE}
     fi
     done
 done
+
+# Finally, merge all csvs into one big file for easy data processing
+# source: https://stackoverflow.com/questions/24641948/merging-csv-files-appending-instead-of-merging/24643455
+OutFileName="all.csv" # Fix the output name
+i=0 # Reset a counter
+for filename in ./*funcs_map.csv; do
+ if [ "$filename" != "$OutFileName" ] ; # Avoid recursion
+ then
+   if [[ $i -eq 0 ]] ; then
+      head -1 "$filename" > "$OutFileName" # Copy header if it is the first file
+   fi
+   tail -n +2 "$filename" >> "$OutFileName" # Append from the 2nd line each file
+   i=$(( $i + 1 )) # Increase the counter
+ fi
+done
