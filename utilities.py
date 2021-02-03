2464e10bf1ee0c5a096fc28340c094bdc089b42c
adarsh <45177460+akalikadien@users.noreply.github.com>
2021-02-03 14:29:56 +0100

    added set_neighbour_bond_distance_search.py to scale bond between atoms and change the element of the neighbour atom

--- utilities.py
+++ utilities.py
@@ -204,6 +204,54 @@ def get_neighbour_bond_distance_search(source_mol_file, central_atom_atomic_num,
     return distance_list
 
 
+def set_neighbour_bond_distance_search(source_mol_file, central_atom_atomic_num, search_this_atomic_num,
+                                       new_length, new_atomic_num):
+    """Use Openbabel's meethods to scale the distance of a specific atom type to a given atom type, the atoms should be
+    bonded and the specific atom type that is searched for should be searched by using it's atomic number.
+    For example, if you have a Mn pincer complex where Br is bonded to Mn and you want to replace Br with H
+    set_neighbour_bond_distance_search(filename, 25, 35, 1.68, 1)
+
+    :param source_mol_file:
+    :param central_atom_atomic_num:
+    :param search_this_atomic_num:
+    :param new_length
+    :param new_atomic_num
+    :return: Same xyz or mol file with atom replaced
+    """
+    central_atom_atomic_num = int(central_atom_atomic_num)
+    search_this_atomic_num = int(search_this_atomic_num)
+
+    # initalize openbabel classes
+    obconversion = openbabel.OBConversion()
+    # both xyz and mol can be used as input but mol contains an accurate graph representation
+    if source_mol_file[-4:] == '.mol':
+        obconversion.SetInFormat('mol')
+    elif source_mol_file[-4:] == '.xyz':
+        obconversion.SetInFormat('xyz')
+    else:
+        raise Exception('file type is incorrect, .mol and .xyz are supported, not', source_mol_file[-4:])
+
+    mol = openbabel.OBMol()
+    obconversion.ReadFile(mol, source_mol_file)
+
+    # iterate over atoms in molecule to search the central atom
+    for atom in openbabel.OBMolAtomIter(mol):
+        atomic_number = atom.GetAtomicNum()
+        if atomic_number == central_atom_atomic_num:  # central atom is found
+            central_atom_vector = np.array([atom.x(), atom.y(), atom.z()])
+            for neighbour_atom in openbabel.OBAtomAtomIter(atom):
+                atomic_number = neighbour_atom.GetAtomicNum()
+                if atomic_number == search_this_atomic_num:  # atom that you were searching for was found
+                    search_this_atom_vector = np.array([neighbour_atom.x(), neighbour_atom.y(), neighbour_atom.z()])
+                    d = neighbour_atom.GetDistance(atom)
+                    distance_vector = (search_this_atom_vector - central_atom_vector)/d
+                    distance_vector = distance_vector / np.linalg.norm(distance_vector)
+                    new_vector = central_atom_vector + distance_vector * new_length
+                    neighbour_atom.SetAtomicNum(new_atomic_num)
+                    neighbour_atom.SetVector(new_vector[0], new_vector[1], new_vector[2])
+                    obconversion.WriteFile(mol, source_mol_file)
+
+
 def convert_list_of_string_to_np_array(array_string):
     """Pandas is importing np arrays as strings, use this converter to convert the list of 1 string to a np.array
     https://stackoverflow.com/questions/42755214/how-to-keep-numpy-array-when-saving-pandas-dataframe-to-csv
